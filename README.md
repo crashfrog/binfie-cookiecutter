bf93f9f5b4a688c800a82f1240d85d6c48cfe245
Justin Payne <crashfrog@gmail.com>
2022-10-20 13:40:22 -0400

    refactor to use setup.cfg

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Python-lite
+# Binfie Project Cookiecutter
 
-A stripped-down Python project cookiecutter.
+A stripped-down Python project cookiecutter for the stuff I usually do. Updated for 2022.
+
+1) Checks PyPI, Bioconda, and Galaxy for project name collision
+2) Installs `click`
+3) Puts in a useful Makefile (`make help`)
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,8 +1,34 @@
 {%- if cookiecutter.project_shell_cmd -%}
 import click
 
+{%- if cookiecutter.use_logging -%}
+import logging
+{% endif %}
+
+## https://click.palletsprojects.com/en/8.1.x/
+
+## @click.group()
+## @click.argument()
+## @click.option('-s', '--string-to-echo', 'string')
+## @click.option('--n', default=1, show_default=True)
+## @click.option("--gr", is_flag=True, show_default=True, default=False, help="Greet the world.")
+## @click.option("--br", is_flag=True, show_default=True, default=True, help="Add a thematic break")
+## @click.option('--shout/--no-shout', default=False)
+
+
+
 @click.command("{{ cookiecutter.project_shell_cmd }}")
-def cli():
+@click.version_option()
+{%- if cookiecutter.use_logging -%}
+@click.option("-v", "--verbose", count=True)
+{% endif %}
+def cli(verbose=0):
+    {%- if cookiecutter.use_logging -%}
+    log_level = {0:60, 1:30, 2:20, 3:10}[verbose]
+    logging.basicConfig(level=log_level,
+                        format='[%(asctime)s][%(name)-12s][%(levelname)-8s] %(message)s',
+                        datefmt='%m-%d %H:%M')
+    {% endif %}
     pass
 
 if __name__ == '__main__':
