b1fa7b0f1aca6e002c9518ef1b7d5e0de4a55805
Justin Payne <crashfrog@gmail.com>
2024-06-03 09:59:52 +0700

    Update __init__.py
    wrong var name

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
@@ -1,4 +1,4 @@
-from .{{cookiecutter.project_slug}} import *
+from .{{cookiecutter.pkg_name}} import *
 import pkg_resources
 
 __version__ = pkg_resources.get_distribution("{{cookiecutter.project_slug}}").version
