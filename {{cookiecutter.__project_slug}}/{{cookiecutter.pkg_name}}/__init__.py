d7f1d96b65f48a96c9712e997d8c6b8db33ba633
Justin Payne <justin.payne@fda.hhs.gov>
2024-10-16 11:04:58 -0400

    edited makefile
    formatting fix
    
    fixed project URLs
    
    bugfixes
    
    bugfix
    
    bugfix

--- {{cookiecutter.__project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.__project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
@@ -1,4 +1,4 @@
 from .{{cookiecutter.pkg_name}} import *
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution("{{cookiecutter.project_slug}}").version
+__version__ = pkg_resources.get_distribution("{{cookiecutter.__project_slug}}").version
